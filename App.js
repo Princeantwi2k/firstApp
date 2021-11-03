efc3bcec1065d0d5ffc81a11b01f4d897f2254b6
princeantwi2k <antwi2k@gmail.com>
2021-11-03 13:19:54 +0800

    added app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image ,TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.pic}>
+        <Image source={require("./assets/ceo.jpg")}  style={{height:300,width: 300,borderRadius:50}}/>
+        
+        </View>
+    
+<View style={styles.input} >
+  <TextInput placeholder="Name" keyboardType="default" style={styles.name} />
+  <TextInput placeholder="email" keyboardType="email-address" style={styles.name} />
+  <TextInput placeholder="password" keyboardType="visible-password" style={styles.name} />
+</View>
+    
     </View>
   );
 }
@@ -14,8 +23,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    marginTop:90,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pic:{
+flex:0.3,
+alignItems:"center",
+justifyContent:'center'
+  },
+  
+
+  input:{
+    flex:1,
+   justifyContent:'center',
+  
+  },
+
+  name:{
+padding:30,
+margin:2,
+borderBottomWidth:2,
+borderBottomColor:'black',
+
+
+width:300
+  }
 });
